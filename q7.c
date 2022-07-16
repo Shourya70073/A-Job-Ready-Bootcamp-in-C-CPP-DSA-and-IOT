eda699b9127145955902ec7e027db17eb714b09d
Shourya70073 <shourya70073@gmail.com>
2022-07-16 13:48:30 +0530

    1st

---
+++ q7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=n*2;i>=1;i--){
+        if(i%2==0){
+            printf("%d",i);
+            printf("\n");
+        }
+    }
+    return 0;
+}
